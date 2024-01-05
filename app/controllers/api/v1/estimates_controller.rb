6683ea6dfa1d5fdc718a4d7162cf897dd5bf0d63
chmen <tony.potapov@gmail.com>
2024-01-05 16:14:48 +0200

    add show action to estimate controller

--- app/controllers/api/v1/estimates_controller.rb
+++ app/controllers/api/v1/estimates_controller.rb
@@ -1,10 +1,20 @@
 module API
   module V1
     class EstimatesController < ApplicationController
+      before_action :set_estimate, only: %i[ show update destroy ]
+
+      def show
+        render json: @estimate
+      end
 
       def index
         estimates = Estimate.all
         render json: estimates
+      end
+
+    private
+      def set_estimate
+        @estimate = Estimate.find(params[:id])
       end
     end
   end
