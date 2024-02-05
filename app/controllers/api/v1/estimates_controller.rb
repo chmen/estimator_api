1f5af772d090b50ecdd8f123d93e843e0a84bd08
chmen <tony.potapov@gmail.com>
2024-02-05 11:35:28 +0200

    fix params whitelisting

--- app/controllers/api/v1/estimates_controller.rb
+++ app/controllers/api/v1/estimates_controller.rb
@@ -3,16 +3,27 @@ module API
     class EstimatesController < ApplicationController
       before_action :set_estimate, only: %i[ show update destroy ]
 
-      def show
-        render json: @estimate
-      end
-
       def index
         estimates = Estimate.all
         render json: estimates
       end
 
+      def show
+        render json: @estimate
+      end
+
+      def create
+        @estimate = Estimate.create(:estimate_params)
+
+        render json: @estimate
+      end
+
     private
+
+      def estimate_params
+        params.fetch(:data).permit(:title)
+      end
+
       def set_estimate
         @estimate = Estimate.find(params[:id])
       end
